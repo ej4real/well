b0a2fe3cb17d62a7d597672965446fd7671b156b
Ernz <ortiz_erniejohn@yahoo.com>
2018-01-07 00:07:25 +0800

    clearing
    getting it cleaned

--- now/accounts/forms.py
+++ now/accounts/forms.py
@@ -53,11 +53,11 @@ class UserAdminChangeForm(forms.ModelForm):
 
 
 class RegisterForm(forms.ModelForm):
+    
     """A form for creating new users. Includes all the required
     fields, plus a repeated password."""
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
-
     class Meta:
         model = User
         fields = ('email', 'full_name',)
